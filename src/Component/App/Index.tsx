7122313fe588ac045bc688d095435cc2d595ea9f
X <qscgyujm@gmail.com>
2020-10-29 13:38:51 +0800

    [FIX] fix folder structer

--- src/Component/App/Index.tsx
+++ src/Component/App/Index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
-import compose from '@Helpers/compose';
+import { compose } from '@Helpers/index';
 
 import Home from '@Component/App/view/Home';
 
